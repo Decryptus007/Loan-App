c16acf003af0cb7ed1d39797e0a5530093466c1a
splashray <olayemi0427@gmail.com>
2022-04-10 10:38:30 +0100

    john changes

--- home.php
+++ home.php
@@ -4,7 +4,7 @@ ob_start();
  session_start();
  include_once('../db_connect.php');
  if (strlen($_SESSION['id']==0)) {
-    $_SESSION['id'];
+  
   header('location:logout.php');
   ob_end_flush();
   } else{
@@ -182,41 +182,85 @@ ob_start();
         </div>
     </div>
 
-    <!-- php loan details -->
-        <?php
-        $userid=$_SESSION['id'];
-        $query=mysqli_query($conn,"SELECT * FROM borrowers WHERE id='$userid'");
-        while($result=mysqli_fetch_array($query))
-        {
-            $_SESSION['fname']= $result['firstname'];
-            $_SESSION['lname']= $result['lastname'];
-            $_SESSION['email']= $result['email'];
+    
 
-            
-        
-                $qry = $conn->query("SELECT * FROM loan_list WHERE borrower_id = '$userid'");
-                    while($row = $type->fetch_assoc()){
-        
-                
-        ?>
+         
     <!-- LOAN DETAILS -->
     <div id="loanDetails" class="loanDetails">
         <div class="overlay" id="overlayL"></div>
         <div class="loanHolder">
             <h2>Loan Details</h2>
+                <?php
+                  $userid = $_SESSION['id'];
+                  $query=mysqli_query($conn,"SELECT * FROM borrowers WHERE id='$userid'");
+                  while($result=mysqli_fetch_array($query))
+                  {
+                      $_SESSION['fname']= $result['firstname'];
+                      $_SESSION['lname']= $result['lastname'];
+                      $_SESSION['email']= $result['email'];
+          
+                  } 
+                $result = $conn->query("SELECT * FROM loan_list WHERE borrower_id = '$userid'");
+              //  $emparray = array();
+                while($row =mysqli_fetch_assoc($result))
+                {
+                  //  $emparray[] = $row;
+                
+
+
+                ?>
             <div class="loanDetail">
-                <p>Name: <span> <?php echo $_SESSION['fname']; ?> </span></p>
-                <p>Reference No: <span>234567432</span></p>
-                <p>Loan Type: <span>Personal Loan</span></p>
-                <p>Loan Plan: <span>27 months</span></p>
-                <p>Loan Amount: <span>NGN 0</span></p>
-                <p>Loan Type: <span>Personal Loan</span></p>
-                <p>Date Released: <span>April 5th</span></p>
-                <p>Loan Status: <span>Pending</span></p>
+                    <!-- php loan details -->
+                <p>Name: <span> <?php echo $_SESSION['fname'].' '. $_SESSION['lname']; ?> </span></p>
+                <p>Reference No: <span><?php echo $row['ref_no']; ?></span></p>
+                <p>Loan Type: <span>
+                    <?php if($row['loan_type_id']=="1"){ 
+                    echo "Small Business";}
+                    elseif ($row['loan_type_id']=="2") {
+                     echo "Mortgages";
+                    }elseif($row['loan_type_id']=="3"){
+                        echo "Personal Loans";
+                    }else {
+                        echo "Not Valid";
+                    } ?>
+                    </span></p>
+                <p>Loan Plan: <span>
+                     <?php if($row['plan_id']=="1"){ 
+                    echo "36 Months";}
+                    elseif ($row['plan_id']=="2") {
+                     echo "24 Months";
+                    }elseif($row['plan_id']=="3"){
+                        echo "27 Months";
+                    }else {
+                        echo "Not Valid";
+                    } ?></span></p>
+                <p>Loan Purpose: <span> <?php echo $row['purpose']; ?></span></p>
+                <p>Loan Amount: <span>NGN <?php echo $row['amount']; ?></span></p>
+                <p>Date Released: <span>
+                <?php if($row['date_released']=="0000-00-00 00:00:00"){ 
+                    echo "Not Available Yet";
+                    }else {
+                        echo $row['date_released'];
+                    } ?>
+                    </span></p>
+                <p>Loan Status: <span>
+                <?php if($row['status'] == 0): ?>
+						 			<span class="badge badge-warning">For Approval</span>
+						 		<?php elseif($row['status'] == 1): ?>
+						 			<span class="badge badge-info">Approved</span>
+					 			<?php elseif($row['status'] == 2): ?>
+						 			<span class="badge badge-primary">Released</span>
+					 			<?php elseif($row['status'] == 3): ?>
+						 			<span class="badge badge-success">Completed</span>
+					 			<?php elseif($row['status'] == 4): ?>
+						 			<span class="badge badge-danger">Denied</span>
+						 		<?php endif; ?>
+                </span></p>
             </div>
+        <?php }       ?>
         </div>
     </div>
-            <?php }} ?>
+          
     <script src="./js/app.js"></script>
 </body>
 
